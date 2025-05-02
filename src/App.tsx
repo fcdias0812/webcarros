d8ea6e36941c19f1b263aea216967e52c62dc1ec
Fabrício Dias <dias.fabricio0812@gmail.com>
2025-05-02 18:59:03 -0300

    Começando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router";
+import { Layout } from "./components/layout";
+
+import { Home } from "./pages/home";
+import { Login } from "./pages/login";
+import { Register } from "./pages/register";
+import { Dashboard } from "./pages/dashboard";
+import { New } from "./pages/dashboard/new";
+import { CarDetail } from "./pages/car";
+
+export const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/car/:id",
+        element: <CarDetail />,
+      },
+      {
+        path: "/dashboard",
+        element: <Dashboard />,
+      },
+      {
+        path: "/dashboard/new",
+        element: <New />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+]);
